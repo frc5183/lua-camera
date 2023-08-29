9ef375eb05b3c4c1933a77898e98f4186e8f3d67
Trip-kun <kaidenmcmartin1@gmail.com>
2023-08-29 19:59:36 -0400

    dasdfnalksjdf

--- src/lua/main.cpp
+++ src/lua/main.cpp
@@ -15,12 +15,17 @@ static int w_activate(lua_State *L) {
     android.activate();
     return 0;
 };
-extern "C" int luaopen_camera(lua_State *L)
+extern "C"
 {
-    lua_newtable(L);
-    lua_pushcfunction(L, w_activate);
-    lua_setfield(L, -2, "activate");
-    lua_pushcfunction(L, w_getpicture);
-    lua_setfield(L, -2, "getPicture");
+static const struct luaL_Reg libs [] = {
+        {"activate", w_activate},
+        {"getPicture", w_getpicture},
+        {NULL, NULL}
+};
+ int luaopen_camera(lua_State *L)
+{
+    luaL_register(L, "camera", libs);
     return 1;
+}
+
 }
